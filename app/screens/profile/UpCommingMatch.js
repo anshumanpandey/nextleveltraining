6935bec27a953d3c626e8e6f417ea24e3d69233d
leo <leo_9_t@hotmail.com>
2020-07-17 13:38:50 -0400

    WORK on add match screen

--- app/screens/profile/UpCommingMatch.js
+++ app/screens/profile/UpCommingMatch.js
@@ -1,59 +1,93 @@
-import React, {useState} from 'react';
-import {View, TextInput, Text, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
-import {Icon} from 'native-base';
+import { Formik } from 'formik';
 import HeaderClosePlus from '../../components/header/HeaderClosePlus';
 import NavigationService from '../../navigation/NavigationService';
 import Dimension from '../../constants/dimensions';
 import DatePicker from 'react-native-datepicker';
+import ErrorLabel from '../../components/ErrorLabel';
+import useAxios from 'axios-hooks'
+import { dispatchGlobalState, GLOBAL_STATE_ACTIONS } from '../../state/GlobalState';
 
 const UpComingMatch = (props) => {
   const cb = props.navigation.state.params.cb;
-  const [teamName, setTeamName] = useState('');
-  const [startDate, setStartDate] = useState('');
+
+
+  const [, postMatch] = useAxios({
+    url: '/Users/SaveUpcomingMatch',
+    method: 'POST'
+  }, { manual: true })
+
+  const [getUserReq, getUserData] = useAxios({
+    url: '/Users/GetUser',
+  }, { manual: true })
+
   return (
-    <View>
-      <HeaderClosePlus
-        isSaveButton={true}
-        saveOnPress={() => {
-          cb({
-            id: 11,
-            title: teamName,
-            startDate,
-          });
-          NavigationService.goBack();
-        }}
-      />
-      <View style={{padding: 30}}>
-        <Text style={styles.titleText}>Playing Against Team Name</Text>
-        <View style={[styles.inputContain,{ marginTop: 0}]}>
-          <TextInput
-            onChangeText={(text) => setTeamName(text)}
-            style={{textAlign: 'left', padding: Dimension.px10, fontSize: 15}}
-            // placeholder="Team Name"
-            // keyboardType="email-address"
-          />
-        </View>
+    <Formik
+      initialValues={{ teamName: '', matchDate: '' }}
+      validate={(values) => {
+        const errors = {}
 
-        <Text style={[styles.titleText,{ marginTop: 20}]}>Match Date</Text>
+        if (!values.teamName) errors.teamName = 'Required'
+        if (!values.matchDate) errors.matchDate = 'Required'
 
-        <View style={[styles.pickerView, {marginTop: Dimension.px15}]}>
-          <DatePicker
-            mode="date"
-            date={startDate}
-            format="DD-MM-YYYY"
-            placeholder={'Select Date'}
-            showIcon={false}
-            customStyles={styles.dateInputs}
-            cancelBtnText="Cancel"
-            confirmBtnText="Confirm"
-            onDateChange={(date) => {
-              setStartDate(date);
-            }}
-          />
-        </View>
-      </View>
-    </View>
+        return errors
+      }}
+      onSubmit={values => {
+        const data = values
+        postMatch({ data })
+        .then(r => getUserData())
+        .then((r) => {
+          dispatchGlobalState({ type: GLOBAL_STATE_ACTIONS.PROFILE, state: r.data})
+          NavigationService.goBack()
+        })
+      }}
+    >
+      {({ handleChange, handleBlur, handleSubmit, setFieldValue, values, errors, touched }) => (
+        <>
+          <View>
+            <HeaderClosePlus
+              isSaveButton={true}
+              saveOnPress={handleSubmit}
+            />
+            <View style={{ padding: 30 }}>
+              <Text style={styles.titleText}>Playing Against Team Name</Text>
+              <View style={[styles.inputContain, { marginTop: 0 }]}>
+                <TextInput
+                  style={{ textAlign: 'left', padding: Dimension.px10, width: Dimension.px100,fontSize: 15 }}
+                  onChangeText={handleChange('teamName')}
+                  onBlur={handleBlur('teamName')}
+                  value={values.teamName}
+                />
+              </View>
+              {errors.teamName && touched.teamName && <ErrorLabel text={errors.teamName} />}
+
+              <Text style={[styles.titleText, { marginTop: 20 }]}>Match Date</Text>
+
+              <View style={[styles.pickerView, { marginTop: Dimension.px15 }]}>
+                <View style={{ width: '100%'}}>
+                <DatePicker
+                  mode="date"
+                  date={values.matchDate}
+                  format="DD-MM-YYYY"
+                  placeholder={'Select Date'}
+                  showIcon={false}
+                  customStyles={styles.dateInputs}
+                  cancelBtnText="Cancel"
+                  confirmBtnText="Confirm"
+                  onDateChange={(_, date) => {
+                    setFieldValue("matchDate",date)
+                  }}
+                />
+                {errors.matchDate && touched.matchDate && <ErrorLabel text={errors.matchDate} />}
+                </View>
+              </View>
+            </View>
+          </View>
+        </>
+      )}
+    </Formik>
   );
 };
 
