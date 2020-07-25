4ac436ed12355c9ba9aa2b2e825e25d99551b680
leo <leo_9_t@hotmail.com>
2020-07-25 13:09:19 -0400

    FOX on order

--- app/screens/profile/CoachProfile.js
+++ app/screens/profile/CoachProfile.js
@@ -1051,6 +1051,16 @@ export const TrainingLocationForm = ({ setSubmitFn, onCreate, navigation, ...par
                             </View>
                             {errors.locationName && touched.locationName && <ErrorLabel text={errors.locationName} />}
 
+                            <NLGooglePlacesAutocomplete
+                                defaultValue={values.address}
+                                onPress={(data, details = null) => {
+                                    setFieldValue("address", data.description)
+                                    setFieldValue("lat", details.geometry.location.lat)
+                                    setFieldValue("lng", details.geometry.location.lng)
+                                }} />
+
+                            {errors.address && touched.address && <ErrorLabel text={errors.address} />}
+
                             <TouchableOpacity onPress={() => {
                                 const options = {
                                     title: 'Select picture',
@@ -1080,16 +1090,6 @@ export const TrainingLocationForm = ({ setSubmitFn, onCreate, navigation, ...par
                             </TouchableOpacity>
                             {values.file && <Image style={{ height: 250, resizeMode: 'contain' }} source={{ uri: values.file?.uri }} />}
                             {errors.file && touched.file && <ErrorLabel text={errors.file} />}
-
-                            <NLGooglePlacesAutocomplete
-                                defaultValue={values.address}
-                                onPress={(data, details = null) => {
-                                    setFieldValue("address", data.description)
-                                    setFieldValue("lat", details.geometry.location.lat)
-                                    setFieldValue("lng", details.geometry.location.lng)
-                                }} />
-
-                            {errors.address && touched.address && <ErrorLabel text={errors.address} />}
 
                             {!setSubmitFn && (
                                 <View style={styles.signup_btn_view}>
