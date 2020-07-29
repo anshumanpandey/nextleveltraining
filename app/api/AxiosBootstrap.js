509bbfcfafd229680a96516940374d68cace4220
leo <leo_9_t@hotmail.com>
2020-07-29 12:42:22 -0400

    FIX on http protocol

--- app/api/AxiosBootstrap.js
+++ app/api/AxiosBootstrap.js
@@ -3,7 +3,7 @@ import Axios from 'axios'
 import { getGlobalState, dispatchGlobalState, GLOBAL_STATE_ACTIONS } from '../state/GlobalState';
 
 export const axiosInstance = Axios.create({
-    baseURL: "https://44.233.116.105/NextLevelTrainingApi/api"
+    baseURL: "http://44.233.116.105/NextLevelTrainingApi/api"
 })
 
 axiosInstance.interceptors.request.use(
