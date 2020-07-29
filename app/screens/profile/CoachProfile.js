5edd05c3be63ba0c9759fbdfe0c805c01de52d27
leo <leo_9_t@hotmail.com>
2020-07-29 16:14:00 -0400

    ADDEd condition

--- app/screens/profile/CoachProfile.js
+++ app/screens/profile/CoachProfile.js
@@ -100,7 +100,9 @@ class MultiStep extends Component {
             this.setState({ selectedSegmentIndex: 4 })
             NavigationService.navigate('Home')
             console.log('step four is completed, navigating to home')
-            dispatchGlobalState({ type: GLOBAL_STATE_ACTIONS.TOGGLE });
+            if (!hasFullProfile(profile)){
+                dispatchGlobalState({ type: GLOBAL_STATE_ACTIONS.TOGGLE });
+            }
             return
         }
 
