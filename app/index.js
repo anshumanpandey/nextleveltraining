00bfd7ada0e6dd9ae99f216fc210635dd8a8f284
leo <leo_9_t@hotmail.com>
2020-07-29 14:03:43 -0400

    FIX onproperty watched

--- app/index.js
+++ app/index.js
@@ -434,7 +434,7 @@ const AppMain = () => {
   useEffect(() => {
     console.log("generationg", screens)
     Apps = createAppContainer(AuthStack);
-  },[token,profile?.Id, profile?.AboutUs,profile?.Achievements,profile?.Teams?.length,profile?.UpcomingMatches?.length,toggle])
+  },[token,profile?.Id, profile?.AboutUs,profile?.Achievements,profile?.Teams?.length,profile?.UpcomingMatches?.length, profile?.BankAccount,toggle])
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
