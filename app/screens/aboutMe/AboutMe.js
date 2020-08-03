eba080e3570af729caf6d3ab3810ee04b44d43d4
leo <leo_9_t@hotmail.com>
2020-08-03 18:08:43 -0400

    FIX on bank account rendering for players

--- app/screens/aboutMe/AboutMe.js
+++ app/screens/aboutMe/AboutMe.js
@@ -16,6 +16,49 @@ const AboutMeScreen = (props) => {
 
     console.log("submitFn", submitFn)
 
+    if (profile.Role == "Player") {
+
+        return (
+            <>
+                <Header
+                    hideCreatePost={true}
+                    toggleDrawer={props.navigation.toggleDrawer}
+                    navigate={props.navigation.navigate}
+                    customButton={() => {
+                        if (currentTab == 0) return <></>
+
+                        return (
+                            <View style={{ flexDirection: 'row', width: '70%', justifyContent: 'flex-end', alignItems: 'center', flexGrow: 1 }}>
+                                {isSaving == true && <Spinner size={28} color="black" style={{ right: 20, position: 'absolute', marginRight: '10%', height: '10%' }} />}
+                                <TouchableOpacity
+                                    disabled={isSaving == true}
+                                    onPress={() => {
+                                        setIsSaving(true)
+                                        if (submitFn) {
+                                            submitFn()
+                                                .then(() => {
+                                                    setIsSaving(false)
+                                                })
+                                        }
+                                    }}>
+                                    <Text style={{ color: 'black', opacity: isSaving == true ? 0.5 : 1, fontSize: 18 }}>Save</Text>
+                                </TouchableOpacity>
+                            </View>
+                        );
+                    }}
+                />
+                {resolveRoleForm(profile, "AboutMe", { player: profile }, (cb) => {
+                    if (cb == null) {
+                        setSubmitFn(null)
+                    } else {
+                        setSubmitFn(cb)
+                    }
+                })}
+            </>
+        );
+
+    }
+
     return (
         <>
             <Header
