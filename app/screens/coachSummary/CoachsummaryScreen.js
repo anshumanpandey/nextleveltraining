eb32b8967b737b94b81fe14db6912d7ff44be5f7
leo <leo_9_t@hotmail.com>
2020-08-04 12:35:57 -0400

    FIx on scroll buttons

--- app/screens/coachSummary/CoachsummaryScreen.js
+++ app/screens/coachSummary/CoachsummaryScreen.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useRef } from 'react'
+import React, { Component, useEffect, useRef, useState } from 'react'
 import { View, Image, FlatList, TouchableWithoutFeedback, Dimensions } from 'react-native'
 import Header from '../../components/header/Header'
 import useAxios from 'axios-hooks'
@@ -10,9 +10,11 @@ import { setHours, parseISO, getHours } from "date-fns"
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
 import NavigationService from '../../navigation/NavigationService'
 import LoaderImage from 'react-native-image-progress';
+import Images from '../../constants/image'
 
 
 const CoachsummaryScreen = (props) => {
+    const [idx, setIdx] = useState(0)
     const listRef = useRef()
     const [profile] = useGlobalState('profile')
     const [{ data, loading, error }, getSummary] = useAxios({
@@ -62,11 +64,20 @@ const CoachsummaryScreen = (props) => {
                 </View>
 
                 <Text style={{ paddingTop: '5%', color: Colors.s_blue, fontSize: 20, fontWeight: 'bold', borderBottomWidth: 1, borderBottomColor: 'rgba(0,0,0,0.2)' }}>
-                    Players Quicks View
+                    Players Quick View
                 </Text>
                 <View style={{ flexDirection: 'row' }}>
                     {data.Players && data.Players.length != 0 && (
-                        <TouchableWithoutFeedback onPress={() => listRef.current?.scrollToOffset({ offset: -Dimensions.get('window').width, animated: true })}>
+                        <TouchableWithoutFeedback onPress={() => {
+                            setIdx(i => {
+                                let newIdx = i - 3
+                                if (newIdx < 0 ) {
+                                    newIdx = 0
+                                }
+                                listRef.current?.scrollToIndex({ index: newIdx, animated: true })
+                                return newIdx
+                            })
+                        }}>
                             <View style={{ justifyContent: 'center' }}>
                                 <Icon type="AntDesign" name="left" />
                             </View>
@@ -74,6 +85,7 @@ const CoachsummaryScreen = (props) => {
                     )}
                     <FlatList
                         style={{ flex: 1 }}
+                        contentContainerStyle={{ flexGrow: 1 }}
                         ref={(r) => listRef.current = r}
                         horizontal={true}
                         showsHorizontalScrollIndicator={false}
@@ -86,20 +98,27 @@ const CoachsummaryScreen = (props) => {
                         keyExtractor={i => i.Id}
                         renderItem={({ item }) => {
                             return (
-                                <View style={{ flexGrow: 1, padding: '5%', flexDirection: 'column', width: '100%' }}>
-                                    <View style={{ width: 62, height: 62, borderRadius: 30, borderColor: Colors.s_blue, borderWidth: 1, }}>
-                                        <LoaderImage
-                                            style={{ width: 60, height: 60, borderRadius: 30, borderColor: 'white', borderWidth: 1, }}
-                                            imageStyle={{ width: 60, height: 60, borderRadius: 30, borderColor: 'white', borderWidth: 1, }}
-                                            source={{ uri: item.ProfileImage }} />
-                                    </View>
+                                <View style={{ flexGrow: 1, padding: '1%', flexDirection: 'column' }}>
+                                    <LoaderImage
+                                        style={{ width: 60, height: 60, borderRadius: 60 / 2, borderColor: Colors.s_blue, borderWidth: 1, }}
+                                        imageStyle={{ width: 60, height: 60, borderRadius: 60 / 2, borderColor: Colors.s_blue, borderWidth: 1, }}
+                                        source={{ uri: item.ProfileImage }} />
                                     <Text>{item.FullName}</Text>
                                 </View>
                             );
                         }}
                     />
                     {data.Players && data.Players.length != 0 && (
-                        <TouchableWithoutFeedback onPress={() => listRef.current?.scrollToOffset({ offset: Dimensions.get('window').width, animated: true })}>
+                        <TouchableWithoutFeedback onPress={() => {
+                            setIdx(i => {
+                                let newIdx = i + 3
+                                if (newIdx > 11) {
+                                    newIdx =  11
+                                }
+                                listRef.current?.scrollToIndex({ index: newIdx, animated: true })
+                                return newIdx
+                            })
+                        }}>
                             <View style={{ marginTop: 'auto', marginBottom: 'auto' }}>
                                 <Icon type="AntDesign" name="right" />
                             </View>
@@ -109,7 +128,7 @@ const CoachsummaryScreen = (props) => {
 
                 <View style={{ paddingTop: '5%', borderBottomWidth: 1, borderBottomColor: 'rgba(0,0,0,0.2)', justifyContent: 'space-between', flexDirection: 'row' }}>
                     <Text style={{ color: Colors.s_blue, fontSize: 20, fontWeight: 'bold' }}>
-                        Upcomming Training
+                        Upcoming Training
                     </Text>
                     <TouchableOpacity onPress={() => NavigationService.navigate("Calendar")}>
                         <Text style={{ color: Colors.s_blue, fontSize: 16, fontWeight: 'bold' }}>
