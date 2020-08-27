a2c299db7876e098217f71aef555c6b86559f175
leo <leo_9_t@hotmail.com>
2020-08-27 17:12:56 -0400

    FIX on verification requirements for coaches

--- app/utils/HasCompletedVerificationProcess.js
+++ app/utils/HasCompletedVerificationProcess.js
@@ -1,9 +1,10 @@
 import IsPlayer from "./perType/IsPlayer";
 import playerProfileIsComplete from "./perType/playerProfileIsComplete";
 import CoachHasCompletedStepFour from "./perType/CoachHasCompletedStepFour";
+import IsCoach from "./perType/IsCoach";
 
 const HasCompletedVerificationProcess = (profile) => {
-    return (IsPlayer(profile) && playerProfileIsComplete(profile)) || CoachHasCompletedStepFour(profile)
+    return (IsPlayer(profile) && playerProfileIsComplete(profile)) || (IsCoach(profile) && profile.AboutUs)
 }
 
 export default HasCompletedVerificationProcess;
