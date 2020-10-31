a5d7c15df55975b66cc7a818dacf99927f01e909
leo <leo_9_t@hotmail.com>
2020-10-31 11:34:08 -0400

    ADDED filtering

--- app/screens/search/Search.js
+++ app/screens/search/Search.js
@@ -14,19 +14,42 @@ import getDistance from 'geolib/es/getDistance';
 
 const NoResultMessage = () => <Text style={{ textAlign: 'center', fontSize: 22, marginTop: '10%' }}>No results</Text>
 
-const FilterModal = ({ isModalVisible, onClose }) => {
-  const initialsValues = { orderBy: null, rate: null }
-  const [filterValues, setFilterValues] = useState({ ...initialsValues, orderBy: 'asc' })
-
-  const starStyles = { flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: '20%'}
+const FilterOptionItem = ({ onPress, selected, styles, children, text }) => {
   const normalStyles = { borderRadius: 25, width: '45%', padding: '3%', marginBottom: '1%', borderColor: '#00000005', borderWidth: 0, color: '#00000040', backgroundColor: '#00000005' }
   const selectedStyles = { borderRadius: 25, width: '45%', padding: '3%', marginBottom: '1%', borderColor: Colors.nl_yellow, borderWidth: 1, color: '#00000095', backgroundColor: `${Colors.nl_yellow}30` }
 
-  const resolveStyles = (selected) => {
-    if (selected) return selectedStyles
-    return normalStyles
-  }
+  return (
+    <TouchableOpacity onPress={onPress} style={[selected ? selectedStyles : normalStyles, { ...styles }]}>
+      {text ?
+        <Text style={{ padding: '1%', textAlign: 'center', color: selected ? selectedStyles.color : normalStyles.color }}>{text}</Text>
+        :
+        children
+      }
+    </TouchableOpacity>
+  );
+}
 
+const StartOptionItem = ({ onPress, selected, value }) => {
+  const starStyles = { flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: '20%' }
+
+  return (
+    <FilterOptionItem onPress={onPress} selected={selected} styles={starStyles}>
+      <Text style={{ padding: '1%', textAlign: 'center' }}>{value}</Text>
+      <Icon
+        type="AntDesign"
+        name="star"
+        style={{
+          fontSize: 14,
+        }}
+      />
+    </FilterOptionItem>
+  );
+}
+
+const initialsValues = { orderBy: null, rate: null, dayOfWeek: [], level: null }
+
+const UseFilters = () => {
+  const [filterValues, setFilterValues] = useState({ ...initialsValues, orderBy: 'asc' })
 
   const setFilterValueFor = (category, val) => {
     setFilterValues(prev => {
@@ -41,97 +64,180 @@ const FilterModal = ({ isModalVisible, onClose }) => {
       }
     })
   }
+  const addValueFor = (category, val) => {
+    const selectedValues = filterValues[category]
+    const uniqueValues = new Set(selectedValues)
+    uniqueValues.has(val) ? uniqueValues.delete(val) : uniqueValues.add(val)
+
+    setFilterValues(prev => ({ ...prev, [category]: Array.from(uniqueValues.values()) }))
+  }
+
+  const toggleValueFor = (category, val) => {
+    setFilterValues(prev => ({ ...prev, [category]: prev[category] == val ? initialsValues[category] : val }))
+  }
+
+  return {
+    filterValues,
+    setFilterValueFor,
+    addValueFor,
+    toggleValueFor
+  }
+}
+
+const FilterModal = ({ isModalVisible, onClose }) => {
+  const { filterValues, setFilterValueFor, addValueFor, toggleValueFor } = UseFilters()
   return (
     <Modal style={{ alignItems: 'center' }} isVisible={isModalVisible} onBackdropPress={() => onClose()}>
-      <View style={{ padding: '3%', flexGrow: 0.6, backgroundColor: 'white', height: '40%', width: '80%' }}>
+      <View style={{ padding: '3%', flexGrow: 0.6, backgroundColor: 'white', minHeight: '85%', width: '80%' }}>
         <Text style={{ fontSize: 24 }}>Filter by</Text>
 
         <View style={{ justifyContent: 'space-around', flex: 1, marginTop: '5%', backgroundColor: '#00000001' }}>
           <View>
             <Text style={{ fontSize: 20, marginBottom: '3%' }}>Distance</Text>
             <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-              <TouchableOpacity onPress={() => setFilterValueFor('orderBy', 'asc')} style={[resolveStyles(filterValues.orderBy == 'asc')]}>
-                <Text style={{ padding: '1%', textAlign: 'center', color: resolveStyles(filterValues.orderBy == 'asc').color }}>Nearest ones</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => setFilterValueFor('orderBy', 'desc')} style={[resolveStyles(filterValues.orderBy == 'desc')]}>
-                <Text style={{ padding: '1%', textAlign: 'center', color: resolveStyles(filterValues.orderBy == 'desc').color }}>Farest ones</Text>
-              </TouchableOpacity>
+              <FilterOptionItem onPress={() => setFilterValueFor('orderBy', 'asc')} selected={filterValues.orderBy == 'asc'} text={"Nearest ones"} />
+              <FilterOptionItem onPress={() => setFilterValueFor('orderBy', 'desc')} selected={filterValues.orderBy == 'desc'} text={"Farest ones"} />
             </View>
           </View>
 
           <View>
-            <Text style={{ fontSize: 20, marginBottom: '3%' }}>Rate</Text>
+            <Text style={{ fontSize: 20, marginBottom: '3%' }}>Availability</Text>
             <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-              <TouchableOpacity onPress={() => setFilterValueFor('rate', 1)} style={[resolveStyles(filterValues.rate == 1), starStyles]}>
-                <Text style={{ padding: '1%' }}>1</Text>
-                <Icon
-                  type="AntDesign"
-                  name="star"
-                  style={{
-                    fontSize: 14,
-                  }}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => setFilterValueFor('rate', 2)} style={[resolveStyles(filterValues.rate == 2), starStyles]}>
-                <Text style={{ padding: '1%', textAlign: 'center' }}>2</Text>
-                <Icon
-                  type="AntDesign"
-                  name="star"
-                  style={{
-                    fontSize: 14,
-                  }}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => setFilterValueFor('rate', 3)} style={[resolveStyles(filterValues.rate == 3), starStyles]}>
-                <Text style={{ padding: '1%', textAlign: 'center' }}>3</Text>
-                <Icon
-                  type="AntDesign"
-                  name="star"
-                  style={{
-                    fontSize: 14,
-                  }}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => setFilterValueFor('rate', 4)} style={[resolveStyles(filterValues.rate == 4), starStyles]}>
-                <Text style={{ padding: '1%', textAlign: 'center' }}>4</Text>
-                <Icon
-                  type="AntDesign"
-                  name="star"
-                  style={{
-                    fontSize: 14,
-                  }}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => setFilterValueFor('rate', 5)} style={[resolveStyles(filterValues.rate == 5), starStyles]}>
-                <Text style={{ padding: '1%', textAlign: 'center' }}>5</Text>
-                <Icon
-                  type="AntDesign"
-                  name="star"
-                  style={{
-                    fontSize: 14,
-                  }}
-                />
-              </TouchableOpacity>
+              <FilterOptionItem
+                onPress={() => addValueFor('dayOfWeek', 'Sunday')}
+                selected={filterValues.dayOfWeek.includes('Sunday')}
+                styles={{ width: '30%' }}
+                text={"Sun"}
+              />
+              <FilterOptionItem
+                onPress={() => addValueFor('dayOfWeek', 'Monday')}
+                selected={filterValues.dayOfWeek.includes('Monday')}
+                styles={{ width: '30%' }}
+                text={"Mon"}
+              />
+              <FilterOptionItem
+                onPress={() => addValueFor('dayOfWeek', 'Tuesday')}
+                selected={filterValues.dayOfWeek.includes('Tuesday')}
+                styles={{ width: '30%' }}
+                text={"Tues"}
+              />
+              <FilterOptionItem
+                onPress={() => addValueFor('dayOfWeek', 'Wednesday')}
+                selected={filterValues.dayOfWeek.includes('Wednesday')}
+                styles={{ width: '30%' }}
+                text={"Wed"}
+              />
+              <FilterOptionItem
+                onPress={() => addValueFor('dayOfWeek', 'Thursday')}
+                selected={filterValues.dayOfWeek.includes('Thursday')}
+                styles={{ width: '30%' }}
+                text={"Thurs"}
+              />
+              <FilterOptionItem
+                onPress={() => addValueFor('dayOfWeek', 'Friday')}
+                selected={filterValues.dayOfWeek.includes('Friday')}
+                styles={{ width: '30%' }}
+                text={"Fri"}
+              />
+              <FilterOptionItem
+                onPress={() => addValueFor('dayOfWeek', 'Saturday')}
+                selected={filterValues.dayOfWeek.includes('Saturday')}
+                styles={{ width: '30%' }}
+                text={"Sat"}
+              />
             </View>
+          </View>
+
+          <View>
+            <Text style={{ fontSize: 20, marginBottom: '3%' }}>Level</Text>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+              <FilterOptionItem
+                onPress={() => toggleValueFor('level', 1)}
+                selected={filterValues.level == 1}
+                text={"Level 1"}
+              />
+
+              <FilterOptionItem
+                onPress={() => toggleValueFor('level', 2)}
+                selected={filterValues.level == 2}
+                text={"Level 2"}
+              />
+
+              <FilterOptionItem
+                onPress={() => toggleValueFor('level', 3)}
+                selected={filterValues.level == 3}
+                text={"Level 3"}
+              />
+
+              <FilterOptionItem
+                onPress={() => toggleValueFor('level', 4)}
+                selected={filterValues.level == 4}
+                text={"Level 4"}
+              />
+
+              <FilterOptionItem
+                onPress={() => toggleValueFor('level', 5)}
+                selected={filterValues.level == 5}
+                text={"Level 5"}
+              />
 
           </View>
         </View>
 
-        <TouchableOpacity
-          onPress={() => {
-            if (filterValues.orderBy) {
-              onClose({ orderBy: filterValues.orderBy })
-            }
-            if (filterValues.rate) {
-              onClose({ rate: filterValues.rate })
-            }
-          }}
-          style={[{ height: 40, backgroundColor: Colors.s_blue, justifyContent: 'center' }]}>
-          <Text style={{ color: 'white', textAlign: 'center', fontSize: 18 }}>Filter</Text>
-        </TouchableOpacity>
+        <View>
+          <Text style={{ fontSize: 20, marginBottom: '3%' }}>Rate</Text>
+          <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+            <StartOptionItem
+              onPress={() => setFilterValueFor('rate', 1)}
+              selected={filterValues.rate == 1}
+              value={1}
+            />
+
+            <StartOptionItem
+              onPress={() => setFilterValueFor('rate', 2)}
+              selected={filterValues.rate == 2}
+              value={2}
+            />
+
+            <StartOptionItem
+              onPress={() => setFilterValueFor('rate', 3)}
+              selected={filterValues.rate == 3}
+              value={3}
+            />
+
+            <StartOptionItem
+              onPress={() => setFilterValueFor('rate', 4)}
+              selected={filterValues.rate == 4}
+              value={4}
+            />
+
+            <StartOptionItem
+              onPress={() => setFilterValueFor('rate', 5)}
+              selected={filterValues.rate == 5}
+              value={5}
+            />
+          </View>
+
+        </View>
+      </View>
+
+      <TouchableOpacity
+        onPress={() => {
+          const res = { dayOfWeek: filterValues.dayOfWeek, level: filterValues.level }
+          if (filterValues.orderBy) {
+            res.orderBy = filterValues.orderBy
+          }
+          if (filterValues.rate) {
+            res.rate = filterValues.rate
+          }
+          onClose(res)
+        }}
+        style={[{ height: 40, backgroundColor: Colors.s_blue, justifyContent: 'center' }]}>
+        <Text style={{ color: 'white', textAlign: 'center', fontSize: 18 }}>Filter</Text>
+      </TouchableOpacity>
 
       </View>
-    </Modal>
+    </Modal >
   );
 }
 
@@ -183,7 +289,7 @@ const Search = (props) => {
   const [currentTab, setCurrentTab] = useState({ i: TabsName[0] == "Coaches" ? 0 : 1 })
 
   const displayFilter = () => {
-    return currentTab.i == 0 && TabsName[0] == "Coaches" || currentTab.i == 1 && TabsName[1] == "Coaches"
+    return (currentTab.i == 0 && TabsName[0] == "Coaches") || (currentTab.i == 1 && TabsName[1] == "Coaches")
   }
 
   const rateFilter = (coach, val) => {
@@ -203,6 +309,41 @@ const Search = (props) => {
 
     if (order == 'asc') return distanceToCoachA - distanceToCoachB
     if (order == 'desc') return distanceToCoachB - distanceToCoachA
+  }
+
+  const parseResults = (users) => {
+    return users
+    .sort((a, b) => {
+      if (!a.Lat || !a.Lng || !b.Lat || !b.Lng || !profile?.Lat || !profile?.Lng) return 0
+
+      if (orderingType.orderBy) {
+        return distanceFilter(a, b, orderingType.orderBy)
+      }
+      return 0
+    })
+    .filter(coach => {
+      if (coach.Role == "Player") return true
+      if (orderingType.rate) {
+        return rateFilter(coach, orderingType.rate)
+      }
+
+      return true
+    })
+    .filter(coach => {
+      if (coach.Role == "Player") return true
+      if (!orderingType.dayOfWeek || orderingType.dayOfWeek.length == 0) return true
+      if (coach.Availabilities.length == 0) return false
+
+      return coach.Availabilities.some(a => orderingType.dayOfWeek.find(dow => a.Day == dow) != null)
+
+    })
+    .filter(coach => {
+      if (coach.Role == "Player") return true
+      if (!orderingType.level) return true
+
+      return coach.Level == orderingType.level
+    })
+
   }
 
   return (
@@ -286,53 +427,18 @@ const Search = (props) => {
       <Tabs onChangeTab={(current) => setCurrentTab(current)} tabBarUnderlineStyle={{ backgroundColor: Colors.nl_yellow }}>
         <Tab textStyle={tabStyle} activeTextStyle={activeTabStyle} tabStyle={{ backgroundColor: 'white' }} activeTabStyle={{ backgroundColor: 'white' }} heading={TabsName[0]}>
           <View style={{ padding: '2%' }}>
-            {searchCoachesReq.data && searchCoachesReq.data[propsToIterate[0]].length == 0 && <NoResultMessage />}
+            {searchCoachesReq.data && parseResults(searchCoachesReq.data[propsToIterate[0]]).length == 0 && <NoResultMessage />}
             {searchCoachesReq.data && searchCoachesReq.data[propsToIterate[0]].length != 0 && <FlatList
               keyExtractor={(item) => item.Id}
-              data={searchCoachesReq.data[propsToIterate[0]]
-                .sort((a, b) => {
-                  if (propsToIterate[0] == "Player" || !a.Lat || !a.Lng || !b.Lat || !b.Lng || !profile?.Lat || !profile?.Lng) return 0
-
-                  if (orderingType.orderBy) {
-                    return distanceFilter(a, b, orderingType.orderBy)
-                  }
-                  return 0
-                })
-                .filter(coach => {
-                  if (propsToIterate[0] == "Player") return true
-                  if (orderingType.rate) {
-                    return rateFilter(coach, orderingType.rate)
-                  }
-
-                  return true
-                })
-              } renderItem={({ item }) => <PostSearchCard hideCoachLevel={true} hideAddress={true} onPress={() => NavigationService.navigate(screensToNavigate[0], { player: item, ...item })} {...item} hideHeartIcon={true} />} />}
+              data={parseResults(searchCoachesReq.data[propsToIterate[0]])} renderItem={({ item }) => <PostSearchCard hideCoachLevel={true} hideAddress={true} onPress={() => NavigationService.navigate(screensToNavigate[0], { player: item, ...item })} {...item} hideHeartIcon={true} />} />}
           </View>
         </Tab>
         <Tab textStyle={tabStyle} activeTextStyle={activeTabStyle} tabStyle={{ backgroundColor: 'white' }} activeTabStyle={{ backgroundColor: 'white' }} heading={TabsName[1]}>
           <View style={{ padding: '2%' }}>
-            {searchCoachesReq.data && searchCoachesReq.data[propsToIterate[1]].length == 0 && <NoResultMessage />}
+            {searchCoachesReq.data && parseResults(searchCoachesReq.data[propsToIterate[1]]).length == 0 && <NoResultMessage />}
             {searchCoachesReq.data && searchCoachesReq.data[propsToIterate[1]].length != 0 && <FlatList
               keyExtractor={(item) => item.Id}
-              data={searchCoachesReq
-                .data[propsToIterate[1]]
-                .sort((a, b) => {
-                  if (propsToIterate[1] == "Player" || !a.Lat || !a.Lng || !b.Lat || !b.Lng || !profile?.Lat || !profile?.Lng) return 0
-
-                  if (orderingType.orderBy) {
-                    return distanceFilter(a, b, orderingType.orderBy)
-                  }
-                  return 0
-                })
-                .filter(coach => {
-                  if (propsToIterate[1] == "Player") return true
-                  if (orderingType.rate) {
-                    return rateFilter(coach, orderingType.rate)
-                  }
-
-                  return true
-                })
-              } renderItem={({ item }) => <PostSearchCard hideAddress={true} onPress={() => NavigationService.navigate(screensToNavigate[1], { player: item, ...item })} {...item} hideHeartIcon={true} />} />}
+              data={parseResults(searchCoachesReq.data[propsToIterate[1]])} renderItem={({ item }) => <PostSearchCard hideAddress={true} onPress={() => NavigationService.navigate(screensToNavigate[1], { player: item, ...item })} {...item} hideHeartIcon={true} />} />}
           </View>
         </Tab>
 
@@ -361,7 +467,7 @@ const Search = (props) => {
         </Tab>
       </Tabs>
       <FilterModal onClose={(ordering) => {
-        setOrderingType(ordering)
+        ordering && setOrderingType(ordering)
         setModalVisible(false)
       }} isModalVisible={isModalVisible} />
     </View>
