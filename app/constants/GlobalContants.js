fc882b417ebdbc3410a656023e9e3289fd2ffedc
leo <leo_9_t@hotmail.com>
2020-10-31 17:45:24 -0400

    FIX on google sign in credentiakls

--- app/constants/GlobalContants.js
+++ app/constants/GlobalContants.js
@@ -1,10 +1,20 @@
-const GlobalContants ={
-    FEATURED_PRICE:'20',
+const GlobalContants = {
+    FEATURED_PRICE: '20',
     BOOKING_COMISSION: 2,
-    PAYPAL_CLIENT_ID:'AWpaMztfwuCn_dP0IYhyvsnGRxgHtgP5GjDOfFi1U21ANbvyr7PRly1iwcplriBjtPuUGnZufVn894cE',
-    PAYPAL_CLIENT_SECRET:'EIzgb7EaeYuczf-7pe633I6nfDroJQ97bix7H56lsDhqGeDIBck7LD1546VuahVCEOosOOvMDC6-ZThb',
+    PAYPAL_CLIENT_ID: 'AWpaMztfwuCn_dP0IYhyvsnGRxgHtgP5GjDOfFi1U21ANbvyr7PRly1iwcplriBjtPuUGnZufVn894cE',
+    PAYPAL_CLIENT_SECRET: 'EIzgb7EaeYuczf-7pe633I6nfDroJQ97bix7H56lsDhqGeDIBck7LD1546VuahVCEOosOOvMDC6-ZThb',
     PAYPAL_TOKEN_URL: 'https://api.paypal.com/v1/oauth2/token',
-    PAYPAL_PAYMENT_URL:'https://api.paypal.com/v1/payments/payment',
+    PAYPAL_PAYMENT_URL: 'https://api.paypal.com/v1/payments/payment',
+    GOOGLE_SIGNIN_DATA: {
+        scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+        webClientId: '669575831507-c3nqa7p8camij0pbo1q5i1m2tp2m5kt0.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+        offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+        hostedDomain: '', // specifies a hosted domain restriction
+        loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+        forceConsentPrompt: false, // [Android] if you want to show the authorization prompt at each login.
+        accountName: '', // [Android] specifies an account name on the device that should be used
+        iosClientId: '669575831507-r84633mtfqc26al0917hep98i2mse110.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    }
 }
 
 /*
--- app/screens/login/Login.js
+++ app/screens/login/Login.js
@@ -24,6 +24,7 @@ import DeviceInfo from 'react-native-device-info';
 import JwtDecode from 'jwt-decode';
 import { RequestDeviceToken } from '../../utils/firebase/RequestDeviceToken';
 import { NavigationActions, StackActions } from 'react-navigation';
+import GlobalContants from '../../constants/GlobalContants';
 
 const Login = (props) => {
   const [role, setRole] = useState();
@@ -56,16 +57,7 @@ const Login = (props) => {
   }
 
   useEffect(() => {
-    GoogleSignin.configure({
-      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-      webClientId: '669575831507-c3nqa7p8camij0pbo1q5i1m2tp2m5kt0.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
-      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-      hostedDomain: '', // specifies a hosted domain restriction
-      loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
-      forceConsentPrompt: false, // [Android] if you want to show the authorization prompt at each login.
-      accountName: '', // [Android] specifies an account name on the device that should be used
-      //iosClientId: '682593494821-0pkvvshud13mpk5q3l3bork71bsm4fed.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
-    });
+    GoogleSignin.configure(GlobalContants.GOOGLE_SIGNIN_DATA);
 
     const focusListener = props.navigation.addListener('didFocus', () => {
       AsyncStorage.getItem('role')
