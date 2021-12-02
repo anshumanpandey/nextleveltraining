a99351c154babccda97a735f1782a68d6c297f3d
APSonin <anshumanpandey71@gmail.com>
2021-12-02 16:46:43 -0400

    REFACTOR apple pay

--- app/utils/askApplePay.js
+++ app/utils/askApplePay.js
@@ -1,24 +1,41 @@
-import ApplePay from "react-native-apple-payment";
+import { Alert } from "react-native";
+
+const { PaymentRequest } = require('react-native-payments');
 
 const askApplePay = async ({ label, amount }) => {
-  const method = {
-    countryCode: "GB",
-    currencyCode: "GBP",
-    supportedNetworks: ["Visa", "MasterCard"],
-    merchantIdentifier: "merchant.Nextlevel"
+  try {
+    const METHOD_DATA = [{
+      supportedMethods: ['apple-pay'],
+      data: {
+        merchantIdentifier: 'Y77A2C426U.merchant.Nextlevel',
+        supportedNetworks: ['visa', 'mastercard', 'amex'],
+        countryCode: 'GB',
+        currencyCode: 'GBP'
+      }
+    }];
+    const DETAILS = {
+      id: 'basic-example',
+      displayItems: [
+        {
+          label,
+          amount: { currency: 'GBP', value: amount }
+        }
+      ],
+      total: {
+        label,
+        amount: { currency: 'GBP', value: amount }
+      }
+    };
+    const paymentRequest = new PaymentRequest(METHOD_DATA, DETAILS);
+    return paymentRequest.show()
+      .then(paymentResponse => {
+        const { transactionIdentifier } = paymentResponse.details;
+        return transactionIdentifier
+      });
+
+  } catch (err) {
+    Alert.alert(err)
+    throw err
   }
-  const details = {
-    total: {
-      label,
-      amount
-    }
-  }
-  const payment = new ApplePay(method, details);
-
-  await payment.canMakePayments()
-
-  const paymentResponse = await payment.initApplePay()
-
-  return paymentResponse
 }
 export default askApplePay;
